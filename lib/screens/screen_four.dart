48d833744b8288e7df9cf37854eb3cbb9f1598d7
TonyoGodson <59567558+TonyoGodson@users.noreply.github.com>
2023-07-09 18:21:42 +0100

    screen four created

---
+++ lib/screens/screen_four.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ScreenFour extends StatefulWidget {
+  const ScreenFour({Key? key}) : super(key: key);
+
+  @override
+  State<ScreenFour> createState() => _ScreenFourState();
+}
+
+class _ScreenFourState extends State<ScreenFour> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+        body:Text("Screen Four"));
+  }
+}
